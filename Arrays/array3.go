6989c668906df78553fc39811146ece7344bb104
PradipDKshirsagar <pradip.kshirsagar@joshsoftware.com>
2019-01-12 22:56:30 +0530

    passing array and new declaration

---
+++ Arrays/array3.go
@@ -0,0 +1,18 @@
+//pass array to functions
+
+package main 
+
+import "fmt"
+
+func Sum(a *[3]float64) (sum float64) {
+    for _, v := range *a {
+        sum += v
+    }
+    return
+}
+
+func main() {
+	array := [...]float64{7.0, 8.5, 9.1} // Another Declartion
+	x := Sum(&array)	
+	fmt.Println(x)
+}
