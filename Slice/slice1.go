49ccd428c4d36973a0ecf4743cf8bccdb60d3173
PradipDKshirsagar <pradip.kshirsagar@joshsoftware.com>
2019-01-12 23:48:43 +0530

    slice intro and underlying array understand

---
+++ Slice/slice1.go
@@ -0,0 +1,33 @@
+// A slice does not store any data, it just describes a section of an underlying array. 
+/*
+A slice is formed by specifying two indices, a low and high bound, separated by a colon:
+
+a[low : high]	
+*/
+package main
+
+import "fmt"
+
+func main() {
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+
+	var s []int = primes[1:4]
+	fmt.Println(s)
+
+	//Slices are like references to arrays
+	names := [4]string{
+		"John",
+		"Paul",
+		"George",
+		"Ringo",
+	}
+	fmt.Println(names)
+
+	a := names[0:2]
+	b := names[1:3]
+	fmt.Println(a, b)
+
+	b[0] = "XXX"
+	fmt.Println(a, b)
+	fmt.Println(names)
+}
