740564fb2af85a8dfe31df80d6a795ed3c46fbdc
PradipDKshirsagar <pradip.kshirsagar@joshsoftware.com>
2019-01-12 19:21:38 +0530

    constants understand

---
+++ Introduction/constants.go
@@ -0,0 +1,15 @@
+package main 
+
+import ( "fmt"
+		 "math")
+
+const s = "constant"
+
+func main() {
+	fmt.Println("s = ", s)
+
+	const n = 12345
+	fmt.Println("n = ", n)
+
+	fmt.Println(math.Sin(n))
+}
