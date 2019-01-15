c1860101fb519d8375ef3cbdd9fa28179e666beb
PradipDKshirsagar <pradip.kshirsagar@joshsoftware.com>
2019-01-15 19:54:35 +0530

    Understand methods in go

--- Methods/method1.go
+++ Methods/method1.go
@@ -7,17 +7,31 @@ In this example, the Abs method has a receiver of type Vertex named v
 */
 package main 
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type Vertex struct {
-	X int 
-	Y int
+	X float64 
+	Y float64
 }
 
-func (v Vertex) Abs() int {
-	return v.X * v.X + 
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+
+func AbsNormal(v Vertex) float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
 func main() {
-	
+	fmt.Println("Method Function : ")
+	v1 := Vertex{3, 4}
+	fmt.Println(v1.Abs())
+
+	fmt.Println("Normal Function : ")
+	v2 := Vertex{3, 4}
+	fmt.Println(AbsNormal(v2))
 }
