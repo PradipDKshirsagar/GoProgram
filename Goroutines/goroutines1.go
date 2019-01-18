d7db14ad90b30a682502d7d24c367b03a3b60a1a
PradipDKshirsagar <pradip.kshirsagar@joshsoftware.com>
2019-01-18 11:40:02 +0530

    How to synchronise the go routines by sync waitgroup in go

---
+++ Goroutines/goroutines1.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg = sync.WaitGroup{}
+
+func main() {
+	var msg = "Hello"
+	wg.Add(1)
+	go func(msg string) {
+		fmt.Println(msg)
+		wg.Done()
+	}(msg)
+	msg = "Goodbye"
+	wg.Wait()
+}
+
+func sayHello() {
+	fmt.Println("hello Pradip")
+}
